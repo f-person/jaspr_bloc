a36739d53778248dd4a020380709c1dc75fdb235
Maksim Zemlyanikin <zemlyanikin.maksim@gmail.com>
2022-09-01 02:45:01 +0400

    Add exports

--- lib/jaspr_bloc.dart
+++ lib/jaspr_bloc.dart
@@ -9,4 +9,6 @@ export 'package:bloc/bloc.dart';
 export './src/bloc_builder.dart';
 export './src/bloc_consumer.dart';
 export './src/bloc_listener.dart';
+export './src/bloc_provider.dart';
 export './src/bloc_selector.dart';
+export './src/multi_bloc_provider.dart';
